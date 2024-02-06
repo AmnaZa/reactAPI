764feb0f41c4c38f11b8527daeeee7692764c8ff
AmnaZa <113046745+AmnaZa@users.noreply.github.com>
2024-02-06 14:34:19 -0500

    Footer added

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+// Footer.js
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer>
+      <div className="logo-container">
+      
+        <h1>My React App</h1>
+      </div>
+     
+    </footer>
+  );
+}
+
+export default Footer;
